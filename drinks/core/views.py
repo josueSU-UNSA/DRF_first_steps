228b75128e04782cdbe6d03ee6efe251999dc6ca
JOSUE_SU <josuesumare18122002@gmail.com>
2023-10-30 00:53:26 -0500

    Displayinh data of our model

--- drinks/core/views.py
+++ drinks/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerializer
 # Create your views here.
+
+def drink_list(request):
+    drink_list_to_serialize=Drink.objects.all()
+    drink_serialized=DrinkSerializer(drink_list_to_serialize,many=True)
+    return JsonResponse(drink_serialized.data,safe=False)
